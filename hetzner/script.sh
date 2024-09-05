fd74733432ac0d9c67644c3adc15cd8ae1ebc967
rasel <rasel@appscode.com>
2024-09-05 18:22:00 +0600

    Add hetzner provider
    Signed-off-by: rasel <rasel@appscode.com>

---
+++ hetzner/script.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+HOME="/root"
+cd ${HOME}
+apt-get -y update
+
+set -xeo pipefail
+
+CLUSTERCTL_VERSION="v1.6.3"
+VCLUSTER_VERSION="v0.20.0-beta.9"
+
+#curl -fsSLO https://github.com/bytebuilders/nats-logger/releases/latest/download/nats-logger-linux-amd64.tar.gz
+#tar -xzvf nats-logger-linux-amd64.tar.gz
+#chmod +x nats-logger-linux-amd64
+#mv nats-logger-linux-amd64 nats-logger
+#SHIPPER_FILE=/root/create-script.log ./nats-logger &
+
+case $(uname -m) in
+    x86_64)
+        sys_arch=amd64
+        ;;
+    arm64 | aarch64)
+        sys_arch=arm64
+        ;;
+    ppc64le)
+        sys_arch=ppc64le
+        ;;
+    s390x)
+        sys_arch=s390x
+        ;;
+    *)
+        sys_arch=amd64
+        ;;
+esac
+opsys=windows
+if [[ "$OSTYPE" == linux* ]]; then
+    opsys=linux
+elif [[ "$OSTYPE" == darwin* ]]; then
+    opsys=darwin
+fi
+timestamp() {
+    date +"%Y/%m/%d %T"
+}
+
+log() {
+    local type="$1"
+    local msg="$2"
+    local script_name=${0##*/}
+    echo "$(timestamp) [$script_name] [$type] $msg"
+}
+
+retry() {
+    local retries="$1"
+    shift
+    local count=0
+    local wait=5
+    until "$@"; do
+        exit="$?"
+        if [ $count -lt $retries ]; then
+            log "INFO" "Attempt $count/$retries. Command exited with exit_code: $exit. Retrying after $wait seconds..."
+            sleep $wait
+        else
+            log "INFO" "Command failed in all $retries attempts with exit_code: $exit. Stopping trying any further...."
+            return $exit
+        fi
+        count=$(($count + 1))
+    done
+    return 0
+}
+
+install_kubectl() {
+    ltral="https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/${opsys}/${sys_arch}/kubectl"
+    local cmnd="curl -LO"
+    retry 5 ${cmnd} ${ltral}
+    ltral="https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/${opsys}/${sys_arch}/kubectl.sha256"
+    cmnd="curl -LO"
+    retry 5 ${cmnd} ${ltral}
+#    echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+    cmnd="install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl"
+    retry 5 ${cmnd}
+}
+
+install_helm() {
+    local cmnd="curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3"
+    retry 5 ${cmnd}
+    chmod 700 get_helm.sh
+    cmnd="./get_helm.sh"
+    retry 5 ${cmnd}
+}
+
+install_clusterctl() {
+    local cmnd="curl -L https://github.com/kubernetes-sigs/cluster-api/releases/download/${CLUSTERCTL_VERSION}/clusterctl-${opsys}-${sys_arch} -o clusterctl"
+    retry 5 ${cmnd}
+    cmnd="install -o root -g root -m 0755 clusterctl /usr/local/bin/clusterctl"
+    retry 5 ${cmnd}
+    clusterctl version
+}
+
+install_vclusterCLI() {
+
+    local cmnd="curl -L -o vcluster https://github.com/loft-sh/vcluster/releases/download/${VCLUSTER_VERSION}/vcluster-${opsys}-${sys_arch}"
+    retry 5 ${cmnd}
+    install -c -m 0755 vcluster /usr/local/bin
+    rm -f vcluster
+
+}
+
+init() {
+    install_kubectl
+    install_helm
+    install_clusterctl
+    install_vclusterCLI
+}
+init
