c8b5e1e56c103bb415bf7aafb63a829189346e99
rasel <rasel@appscode.com>
2024-09-09 18:50:07 +0600

    Add azure image
    Signed-off-by: rasel <rasel@appscode.com>

---
+++ azure/README.md
@@ -0,0 +1,9 @@
+# Azure
+
+* `Kubectl`
+* `Helm`
+* `Clusterctl v1.6.3`
+* `capi-config`
+* `infrustructure config files` `/home/ubuntu/assets/config.yaml`
+
+Size: 182 MB
---
+++ azure/script.sh
@@ -0,0 +1,218 @@
+#!/bin/bash
+
+sudo su
+HOME="/home/ubuntu"
+cd /root
+
+set -eou pipefail
+
+export AZURE_SUBSCRIPTION_ID=" {{ .azure_subscription_id }}"
+export AZURE_TENANT_ID="{{ .azure_tenant_id }}"
+export AZURE_CLIENT_ID="{{ .azure_client_id }}"
+export AZURE_CLIENT_SECRET="{{ .azure_client_secret }}"
+export CLUSTER="{{ .cluster }}"
+export WORKER_MACHINE_COUNT={{ .worker_machine_count }}
+export AZURE_NODE_MACHINE_TYPE="{{ .azure_node_machine_type }}"
+export KUBERNETES_VERSION="v{{ .kubernetes_version }}"
+export AZURE_LOCATION="{{ .azure_location }}"
+export VNET_CIDR="{{ .vnet_cidr }}"
+
+# for logs...
+export NATS_CREDS="{{ .nats_creds }}"
+export NATS_SERVER="{{ .nats_server }}"
+export SHIPPER_SUBJECT="{{ .shipper_subject }}"
+
+export EXP_MACHINE_POOL=true
+export EXP_AKS=true
+export CLUSTER_TOPOLOGY=true
+
+rollback() {
+    kubectl delete cluster $CLUSTER_NAME -n ${CLUSTER_NAMESPACE}
+}
+
+function finish {
+    result=$?
+    if [ $result -ne 0 ]; then
+        rollback || true
+    fi
+
+    if [ $result -ne 0 ]; then
+        echo "Cluster provision: Task failed !"
+    else
+        echo "Cluster provision: Task completed successfully !"
+    fi
+
+    sleep 5s
+
+    [ ! -f /tmp/result.txt ] && echo $result >/tmp/result.txt
+}
+trap finish EXIT
+
+curl -fsSLO https://github.com/bytebuilders/nats-logger/releases/latest/download/nats-logger-linux-amd64.tar.gz
+tar -xzvf nats-logger-linux-amd64.tar.gz
+chmod +x nats-logger-linux-amd64
+mv nats-logger-linux-amd64 nats-logger
+
+exec >/root/create-script.log 2>&1
+SHIPPER_FILE=/root/create-script.log ./nats-logger &
+
+# for generating infrastructure component
+CLUSTER_API_VERSION="{{ .cluster_api_version }}"
+INFRASTRUCTURE_VERSION="{{ .infrastructure_version }}"
+
+PROVIDER_NAME=azure
+SERVICE_NAME=aks
+
+export CLUSTER_NAME=${CLUSTER}
+export CLUSTER_IDENTITY_NAME=${CLUSTER_NAME}
+export AZURE_CLUSTER_IDENTITY_SECRET_NAME="${CLUSTER_NAME}-secret"
+export AZURE_CLUSTER_IDENTITY_SECRET_NAMESPACE=${CLUSTER_NAMESPACE}
+export SUBNET_CIDR=${VNET_CIDR}
+
+case $(uname -m) in
+    x86_64)
+        sys_arch=amd64
+        ;;
+    arm64 | aarch64)
+        sys_arch=arm64
+        ;;
+    ppc64le)
+        sys_arch=ppc64le
+        ;;
+    s390x)
+        sys_arch=s390x
+        ;;
+    *)
+        sys_arch=amd64
+        ;;
+esac
+
+#opearating system
+opsys=windows
+if [[ "$OSTYPE" == linux* ]]; then
+    opsys=linux
+elif [[ "$OSTYPE" == darwin* ]]; then
+    opsys=darwin
+fi
+
+function timestamp() {
+    date +"%Y/%m/%d %T"
+}
+
+function log() {
+    local type="$1"
+    local msg="$2"
+    local script_name=${0##*/}
+    echo "$(timestamp) [$script_name] [$type] $msg"
+}
+
+function retry {
+    local retries="$1"
+    shift
+
+    local count=0
+    local wait=5
+    until "$@"; do
+        exit="$?"
+        if [ $count -lt $retries ]; then
+            log "INFO" "Attempt $count/$retries. Command exited with exit_code: $exit. Retrying after $wait seconds..."
+            sleep $wait
+        else
+            log "INFO" "Command failed in all $retries attempts with exit_code: $exit. Stopping trying any further...."
+            return $exit
+        fi
+        count=$(($count + 1))
+    done
+    return 0
+}
+
+install_kubectl() {
+    echo "--------------installing kubectl--------------"
+    ltral="https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/${opsys}/${sys_arch}/kubectl"
+    local cmnd="curl -LO"
+    retry 5 ${cmnd} ${ltral}
+
+    ltral="https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/${opsys}/${sys_arch}/kubectl.sha256"
+    cmnd="curl -LO"
+    retry 5 ${cmnd} ${ltral}
+
+    echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+
+    cmnd="install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl"
+    retry 5 ${cmnd}
+}
+
+install_helm() {
+    echo "--------------installing helm--------------"
+    local cmnd="curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3"
+    retry 5 ${cmnd}
+
+    chmod 700 get_helm.sh
+
+    cmnd="./get_helm.sh"
+    retry 5 ${cmnd}
+}
+
+
+
+install_clusterctl() {
+    local cmnd="curl -L https://github.com/kubernetes-sigs/cluster-api/releases/download/${CLUSTERCTL}/clusterctl-${opsys}-${sys_arch} -o clusterctl"
+    retry 5 ${cmnd}
+
+    cmnd="install -o root -g root -m 0755 clusterctl /usr/local/bin/clusterctl"
+    retry 5 ${cmnd}
+
+    clusterctl version
+}
+
+generate_infrastructure_config_files() {
+
+    echo "-----------generating infrastructure configuration files--------------"
+
+    # folder structure: {basepath}/{provider-name}/{version}/{components.yaml}
+    mkdir -p ${HOME}/assets/infrastructure-${PROVIDER_NAME}/${INFRASTRUCTURE_VERSION} /root/assets/bootstrap-kubeadm/${CLUSTER_API_VERSION} /root/assets/cluster-api/${CLUSTER_API_VERSION} /root/assets/control-plane-kubeadm/${CLUSTER_API_VERSION}
+
+    wget -P ${HOME}/assets/cluster-api/${CLUSTER_API_VERSION} https://cdn.appscode.com/files/cluster-api/${CLUSTER_API_VERSION}/core-components.yaml
+    wget -P ${HOME}/assets/cluster-api/${CLUSTER_API_VERSION} https://cdn.appscode.com/files/cluster-api/${CLUSTER_API_VERSION}/metadata.yaml
+    wget -P ${HOME}/assets/bootstrap-kubeadm/${CLUSTER_API_VERSION} https://cdn.appscode.com/files/cluster-api/${CLUSTER_API_VERSION}/bootstrap-components.yaml
+    wget -P ${HOME}/assets/bootstrap-kubeadm/${CLUSTER_API_VERSION} https://cdn.appscode.com/files/cluster-api/${CLUSTER_API_VERSION}/metadata.yaml
+    wget -P ${HOME}/assets/control-plane-kubeadm/${CLUSTER_API_VERSION} https://cdn.appscode.com/files/cluster-api/${CLUSTER_API_VERSION}/control-plane-components.yaml
+    wget -P ${HOME}/assets/control-plane-kubeadm/${CLUSTER_API_VERSION} https://cdn.appscode.com/files/cluster-api/${CLUSTER_API_VERSION}/metadata.yaml
+    wget -P ${HOME}/assets/infrastructure-${PROVIDER_NAME}/${INFRASTRUCTURE_VERSION} https://cdn.appscode.com/files/cluster-api-provider-${PROVIDER_NAME}/${INFRASTRUCTURE_VERSION}/infrastructure-components.yaml
+    wget -P ${HOME}/assets/infrastructure-${PROVIDER_NAME}/${INFRASTRUCTURE_VERSION} https://cdn.appscode.com/files/cluster-api-provider-${PROVIDER_NAME}/${INFRASTRUCTURE_VERSION}/cluster-template-${SERVICE_NAME}.yaml
+    wget -P ${HOME}/assets/infrastructure-${PROVIDER_NAME}/${INFRASTRUCTURE_VERSION} https://cdn.appscode.com/files/cluster-api-provider-${PROVIDER_NAME}/${INFRASTRUCTURE_VERSION}/metadata.yaml
+
+    cat <<EOF >${HOME}/assets/config.yaml
+providers:
+  - name: "cluster-api"
+    type: "CoreProvider"
+    url: "${HOME}/assets/cluster-api/$CLUSTER_API_VERSION/core-components.yaml"
+  - name: "kubeadm"
+    type: "BootstrapProvider"
+    url: "${HOME}/assets/bootstrap-kubeadm/$CLUSTER_API_VERSION/bootstrap-components.yaml"
+  - name: "kubeadm"
+    type: "ControlPlaneProvider"
+    url: "${HOME}/assets/control-plane-kubeadm/$CLUSTER_API_VERSION/control-plane-components.yaml"
+  - name: "${PROVIDER_NAME}"
+    type: "InfrastructureProvider"
+    url: "${HOME}/assets/infrastructure-${PROVIDER_NAME}/$INFRASTRUCTURE_VERSION/infrastructure-components.yaml"
+overridesFolder: "${HOME}/assets"
+EOF
+}
+
+#capi-config-linux-amd64 capz <./cluster.yaml >./configured-cluster.yaml
+install_capi-config() {
+    curl -fsSLO https://github.com/bytebuilders/capi-config/releases/download/v0.0.1/capi-config-linux-amd64.tar.gz
+    tar -xzf capi-config-linux-amd64.tar.gz
+    cp capi-config-linux-amd64 /bin
+}
+
+init() {
+    install_helm
+    install_kubectl
+    install_clusterctl
+    install_capi-config
+    generate_infrastructure_config_files
+}
+
+init
