f4aa3870ac9a7db4b5c96752fcde1a93cf8afd9e
RejwankabirHamim <hamim@appscode.com>
2025-01-03 16:22:42 +0600

    Add KubeVirt
    Signed-off-by: RejwankabirHamim <hamim@appscode.com>

---
+++ capk/script.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+set -xeo pipefail
+
+case $(uname -m) in
+    x86_64)
+        sys_arch=amd64
+        ;;
+    arm64 | aarch64)
+        sys_arch=arm64
+        ;;
+    ppc64le)
+        sys_arch=ppc64le
+        ;;
+    s390x)
+        sys_arch=s390x
+        ;;
+    *)
+        sys_arch=amd64
+        ;;
+esac
+opsys=windows
+if [[ "$OSTYPE" == linux* ]]; then
+    opsys=linux
+elif [[ "$OSTYPE" == darwin* ]]; then
+    opsys=darwin
+fi
+
+timestamp() {
+    date +"%Y/%m/%d %T"
+}
+
+log() {
+    local type="$1"
+    local msg="$2"
+    local script_name=${0##*/}
+    echo "$(timestamp) [$script_name] [$type] $msg"
+}
+
+retry() {
+    local retries="$1"
+    shift
+    local count=0
+    local wait=5
+    until "$@"; do
+        exit="$?"
+        if [ $count -lt $retries ]; then
+            log "INFO" "Attempt $count/$retries. Command exited with exit_code: $exit. Retrying after $wait seconds..."
+            sleep $wait
+        else
+            log "INFO" "Command failed in all $retries attempts with exit_code: $exit. Stopping trying any further...."
+            return $exit
+        fi
+        count=$(($count + 1))
+    done
+    return 0
+}
+
+install_nats-logger() {
+    curl -fsSLO https://github.com/appscode-cloud/nats-logger/releases/download/v0.0.6/nats-logger-linux-amd64.tar.gz
+    tar -xzvf nats-logger-linux-amd64.tar.gz
+    chmod +x nats-logger-linux-amd64
+    mv nats-logger-linux-amd64 /bin/nats-logger
+}
+
+install_kubectl() {
+    ltral="https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/${opsys}/${sys_arch}/kubectl"
+    local cmnd="curl -LO"
+    retry 5 ${cmnd} ${ltral}
+    ltral="https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/${opsys}/${sys_arch}/kubectl.sha256"
+    cmnd="curl -LO"
+    retry 5 ${cmnd} ${ltral}
+    #    echo "$(cat kubectl.sha256)  kubectl" | sha256sum -c
+    cmnd="install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl"
+    retry 5 ${cmnd}
+}
+
+install_helm() {
+    local cmnd="curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3"
+    retry 5 ${cmnd}
+    chmod 700 get_helm.sh
+    cmnd="./get_helm.sh"
+    retry 5 ${cmnd}
+}
+
+install_clusterctl() {
+    local cmnd="curl -L https://github.com/kubernetes-sigs/cluster-api/releases/download/${CLUSTERCTL}/clusterctl-${opsys}-${sys_arch} -o clusterctl"
+    retry 5 ${cmnd}
+    cmnd="install -o root -g root -m 0755 clusterctl /usr/local/bin/clusterctl"
+    retry 5 ${cmnd}
+    clusterctl version
+}
+
+#capi-config-linux-amd64 capz <./cluster.yaml >./configured-cluster.yaml
+install_capi-config() {
+    curl -fsSLO https://github.com/bytebuilders/capi-config/releases/download/v0.0.1/capi-config-linux-amd64.tar.gz
+    tar -xzf capi-config-linux-amd64.tar.gz
+    cp capi-config-linux-amd64 /bin
+}
+
+init() {
+    install_nats-logger
+    install_kubectl
+    install_helm
+    install_clusterctl
+    install_capi-config
+}
+init
